25871e54e3690a4295cc39b11f8d617ec6cec6f3
anlcan <anilcanbaykal@gmail.com>
2019-04-23 17:25:28 +0200

    added altervista support

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ This project is a consist of:
 - a simple Apple Script for automator to add synonym lookup service
 
     ![Alt Text](img/selection.png) ![Alt Text](img/list.png)
+
 - a python script that send usage to firebase which is later displayed on a website
+
    ![Alt Text](img/collections.png)
--- synonym.py
+++ synonym.py
@@ -3,12 +3,15 @@ from sys import argv
 
 from dotenv import load_dotenv
 from requests import get
+from functools import reduce
+from operator import concat
+from re import sub
 
 load_dotenv()
-HOST = "http://words.bighugelabs.com/api/2/"
 
 
-def run(input):
+def bighugelabs(input):
+    HOST = "http://words.bighugelabs.com/api/2/"
     URL = HOST + "/".join([getenv("BIGHUGELABS_KEY"), input, "json"])
     response = get(URL)
     if response.status_code != 200:
@@ -23,5 +26,35 @@ def run(input):
                 print(syn)
 
 
+def thesaurus_altervista(input):
+    """
+    thesaurus.altervista.
+    :param input:
+    :return:
+    """
+    HOST = "http://thesaurus.altervista.org/thesaurus/v1"
+    params = {
+        "language": "en_US",
+        "key": getenv("THESAURUS_KEY"),
+        "word": input,
+        "output": "json"
+    }
+    response = get(HOST, params=params)
+
+    if response.status_code != 200:
+        return
+
+    answer = response.json()
+    p = [x['list']['synonyms'].split("|") for x in answer['response']]
+    k = reduce(concat, p)
+    # k = [x.replace(' (generic term)', '') for x in k]
+    k = [sub(" (.*)", '', x) for x in k]
+    syn = list(dict.fromkeys(k))  # replace duplicates
+
+    for s in syn:
+        print(s)
+
+
 if __name__ == "__main__":
-    run(argv[1])
+    #bighugelabs(argv[1])
+    thesaurus_altervista(argv[1])
